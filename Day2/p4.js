5f02d5f8dc15c73e7e39c9e2c39131c3e653628f
thevishalcode <thevishalcode@gmail.com>
2025-02-24 11:40:24 +0530

    Done

--- Day2/p4.js
+++ Day2/p4.js
@@ -1,4 +1,85 @@
 const http=require('http');
-const server=http.createServer((req,res)=>{
+const fs=require('fs/promises');
+const server=http.createServer(async(req,res)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/html');
+    const data=await fs.readFile('./index.html','utf-8');
+    res.end(data);
+})
+server.listen(9000,()=>{
+    console.log("Server is running on port 9000");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 })
